a0f3eac2142fd9930b23dad31b94b56bed0337e5
yutounun <geek.yuto@gmail.com>
2025-02-28 21:40:52 -0800

    change api.js to api.ts

---
+++ src/libs/api.ts
@@ -0,0 +1,39 @@
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { Todo } from "../types/todo";
+import supabase from "./supabaseClient";
+
+export async function fetchTodos() {
+  const { data, error }: PostgrestSingleResponse<Todo[]> = await supabase
+    .from("todos")
+    .select("*");
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+  return { data, error };
+}
+
+export async function createTodo(label: string) {
+  const { data, error }: PostgrestSingleResponse<Todo[]> = await supabase
+    .from("todos")
+    .insert({ label })
+    .select();
+  return { data: data?.[0], error };
+}
+
+export async function updateTodo(id: number, label: string) {
+  const { data, error } = await supabase
+    .from("todos")
+    .update({ label })
+    .eq("id", id);
+  return { data, error };
+}
+
+export async function deleteTodo(id: number) {
+  const { data, error } = await supabase
+    .from("todos")
+    .delete()
+    .eq("id", id)
+    .select();
+  return { data, error };
+}
